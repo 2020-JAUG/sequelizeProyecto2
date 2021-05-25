8627b229521f0cff5463348099b0424c51e1f6b4
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-25 10:14:29 +0200

    Conexion a la data base funcionando, mas servidor simultaneamente

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port =  3000;
+const db = require("./db.js");
 
-//Starting server
-app.listen(port, () => console.log(`Node server running on http://localhost:${port}`));
+
+//Conecting to the database
+db.then(() => {
+
+    //Starting server
+    app.listen(port, () => console.log(`Node server running on http://localhost:${port}`));
+})
+.catch((err)=> console.log(err.message));
