d8c208a1e69e4d2cbdf01d1a1361ee0b61b935d7
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-28 09:39:56 +0200

    Instancio cors

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const port =  3000;
 const db = require("./db.js");
 const router = require("./router");
+const cors = require("cors");
 
 //Middleware
+app.use(cors());//Para poder levantar dos servidores en local, cuando trabajamos con front y back
 app.use(express.json());
 app.use(router);
 
