a604653a5b2986157bceb0a2f7e45dd2631e6db8
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-28 09:41:30 +0200

    Instanciamos login

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,7 +1,33 @@
-const router = require("express").Router();
-const loginController = require("./login.controller");
-const userController = reuire("../controllers/user.controller")
+const pasajeroController = require('./pasajero.controller');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = "Wayco tiene futbolin";
 
 class LoginController {
-    
+
+    async validate(nombreCheck,passwordCheck){
+
+        let pasajero = await pasajeroController.namePassenger(nombreCheck);
+
+        let password = pasajero.password;
+
+        let verificar = await bcrypt.compare(passwordCheck,password);
+
+        if(!verificar) {
+            return new Error("El password y el email no coinciden");
+
+        }
+
+        let payload = {
+            pasajeroId : pasajero.id,
+            createdAt: new Date,
+            isAdmin: pasajero.isAdmin
+        };
+
+        return jwt.sign(payload,secret);
+
+    }
 }
+
+let loginController = new LoginController();
+module.exports = loginController;
