a197620bcf443af6b44b7f074536c7e72daaa44e
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-29 16:07:43 +0200

    Corrijo el metodo authenticate

--- middleware/admin.js
+++ middleware/admin.js
@@ -6,24 +6,26 @@ const admin = (req, res, next) => {
 
     try {
 
+        if(!req.headers.authorization) {
+            return "No tienes token";
+        }
+
         let token = require.headers.authenticate.split(" ")[1];
         let auth = jwt.verify(token, secret);
 
-        if(( auth.pasajeroId != req.body.id ) && (auth.isAdmin == req.body.isAdmin)){
+        if( auth.isAdmin == false ) {
 
-            throw new Error("No tienes permiso.");
+            throw new Error("No tienes permisos.");
         }
 
         return next();
 
-    } catch (error) {
+    }catch (error) {
 
             res.status(500).json({
                 message: error.message
             });
     }
 }
-
-
 
 module.exports = admin;
