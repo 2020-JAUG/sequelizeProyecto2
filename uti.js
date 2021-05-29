ffd873a552cea74dabd9daa1e380fa5ca04e5398
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-29 16:07:16 +0200

    Creo archivo utilidades

---
+++ uti.js
@@ -0,0 +1,30 @@
+
+const comprobar = (data, type) => {
+
+    switch(type) {
+
+        case "string":
+
+
+        break;
+
+        case "number":
+
+
+        break;
+
+        case "email":
+
+
+        break;
+
+        case "date":
+
+
+        break;
+
+        default:
+    }
+}
+
+module.exports = comprobar;
