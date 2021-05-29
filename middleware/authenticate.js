b1ff4dd114e046a3362e2f6e7bf28099c2c63245
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-29 16:08:04 +0200

    fixed authenticate

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,13 +5,24 @@ const secret = "Wayco tiene futbolin";
 const authenticate = (req, res, next) => {
     try {
 
+        if( !req.headers.authorization ) {
+            return new Error("No tienes token");
+        }
+
         let token = require.headers.authenticate.split(" ")[1];
         let auth = jwt.verify(token, secret);
 
-        if( auth.pasajeroId != req.params.id )
-        throw new Error("No tienes permiso.");
+        if( auth.isAdmin == true ) {
 
-        return next();
+            return next();
+        } else {
+            if( auth.pasajeroId != req.body.id && auth.pasajeroId != req.body.pasajeroId ) {
+
+                throw new Error("No tienes permiso.");
+            } else {
+                return next();
+            }
+        }
 
     } catch (error) {
         res.status(500).json({
